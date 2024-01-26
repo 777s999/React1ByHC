82d6a692d0b2d1573363186d85ffb4f34ae16dfd
Subrata Rout <subrataa916@gmail.com>
2024-01-26 15:20:04 +0530

    Committing changes in 01vitereact/src/main.jsx and customReact/customReact.js

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function MyApp() {
+  return (
+    <div>
+      <h1>My custom React </h1>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<MyApp />);
